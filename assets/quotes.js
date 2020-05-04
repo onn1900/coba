a50f75caea0fea5f9194fcc870d3fb45adf7197a
onn1900 <64752383+onn1900@users.noreply.github.com>
2020-05-04 11:47:36 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,11 +2,11 @@
 export default [
     rettier-ignore
 export default [
-    { author: "BBLACKFRCD", quotes: "Ojo lali ambekan" },
-    { author: "BBLACKFRCD", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "BBLACKFRCD", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "BBLACKFRCD", quotes: "I love you hiii geliii." },
-    { author: "BBLACKFRCD", quotes: "NOVITA HALU." },
-    { author: "BBLACKFRCD", quotes: "NOVITA GENDENG." },
+    { author: "Selamat malam, Rehatkan rindu yang terpendam, Biar mimpi manis merawat dendam, Dibuai lena yang panjang ~nvtrmdfrds~"", quotes: "Ojo lali ambekan" },
+    { author: "Selamat malam, Rehatkan rindu yang terpendam, Biar mimpi manis merawat dendam, Dibuai lena yang panjang ~nvtrmdfrds~"", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "Selamat malam, Rehatkan rindu yang terpendam, Biar mimpi manis merawat dendam, Dibuai lena yang panjang ~nvtrmdfrds~"", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "Selamat malam, Rehatkan rindu yang terpendam, Biar mimpi manis merawat dendam, Dibuai lena yang panjang ~nvtrmdfrds~"", quotes: "everything was beautiful🥰" },
+    { author: "Selamat malam, Rehatkan rindu yang terpendam, Biar mimpi manis merawat dendam, Dibuai lena yang panjang ~nvtrmdfrds~"", quotes: "NOVITA HALU." },
+    { author: "Selamat malam, Rehatkan rindu yang terpendam, Biar mimpi manis merawat dendam, Dibuai lena yang panjang ~nvtrmdfrds~"", quotes: "NOVITA GENDENG." },
     { author: "Selamat malam, Rehatkan rindu yang terpendam, Biar mimpi manis merawat dendam, Dibuai lena yang panjang ~nvtrmdfrds~", quotes: "NOVITA PALING GENDENG" },
 ];
